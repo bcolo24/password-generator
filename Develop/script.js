8bf0ddb8d384b7a101822820a94aa6f9d1511592
Bethany Colon <bethany.colon28@gmail.com>
2023-10-12 22:38:38 -0400

    moved index.html

--- Develop/script.js
+++ Develop/script.js
@@ -3,8 +3,8 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 var CharArr = [];
+
 
 var UppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var UpperCharArr = UppercaseLetters.split("");
@@ -19,9 +19,9 @@ var Symbols = '!@#$%^&*()_-+=<>?/';
 var SymbolsArr = Symbols.split("");
 
 function generatePassword() {  
-
-  var NumofChar = parseInt(prompt("How many characters between 8-128 would you like in your password?"));
-  var passwordArr = []
+  
+  var NumofChar = parseInt(prompt("How many characters between 8 and 128 would you like in your password?"));
+  var passwordArr = [];
 
   if (NumofChar < 8 || NumofChar > 128) {
     alert("Please enter a number between 8 and 128.");
@@ -34,57 +34,39 @@ function generatePassword() {
     var RandomSymbol = confirm("Do you want symbols in your password?");
   }
 
-  if (RandomUpper){
-    CharArr = CharArr.push(UpperCharArr);
-  }
+  CharArr = [];
 
-  // if (RandomUpper) {
-  //   var RandomUp = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  //  return RandomUp
-  // } 
+  if (RandomUpper){
+    CharArr = CharArr.concat(UpperCharArr);
+  }
 
  if (RandomLower) {
-  CharArr = CharArr.push(LowerCharArr);
-  //return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    CharArr = CharArr.concat(LowerCharArr);
   } 
 
-  
   if (RandomNumber) {
-    CharArr = CharArr.push(NumbersArr);
-    return //String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    CharArr = CharArr.concat(NumbersArr);
   }
-
-
+  
   if (RandomSymbol) {
-    CharArr = CharArr.push(SymbolsArr);
-    return //constsymbol = ".@#$%^&*()<>?/-+=!,";
+    CharArr = CharArr.concat(SymbolsArr);
   }
-  // console.log("Hey! You clicked the button!");
 
-// everything will go here! 
-// 1. Prompt the user for the password criteria
-    //  a. password length between 8 - 128
-    // b. lowercase, uppercase, numbers, special characters
-// 2. Validate the input- at least one character type selected
-// 3. Generate password
-// 4. Display the generated password on the page
+  for (var i = 0; i < NumofChar; i++) {
+  var chosenChar = (Math.floor(Math.random() * NumofChar.length));
+  passwordArr.concat(chosenChar);
+  }
 
-  // return "Generated password will go here.";
+  return chosenChar;
 }
+
 
 // Write password to the #password input
-
-for (var i = 0; i < NumofChar; i++) {
-  var chosenChar = (Math.floor(Math.random() * NumofChar));
-  passwordArr.push(chosenChar)
-}
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
